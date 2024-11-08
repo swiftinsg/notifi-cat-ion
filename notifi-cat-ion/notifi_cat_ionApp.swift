0ae7d33caf540cbaafb9bf8e13b1aed97b67974b
Jia Chen <github@jiachen.app>
2024-11-08 19:17:08 +0800

    Initial Commit

---
+++ notifi-cat-ion/notifi_cat_ionApp.swift
@@ -0,0 +1,17 @@
+//
+//  notifi_cat_ionApp.swift
+//  notifi-cat-ion
+//
+//  Created by Jia Chen Yee on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct notifi_cat_ionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
