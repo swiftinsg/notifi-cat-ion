56b1888567af3e9ebd7d7e34a939423e27ca2452
Jia Chen <github@jiachen.app>
2024-11-08 20:17:46 +0800

    feat: add user notification action handler

--- notifi-cat-ion/notifi_cat_ionApp.swift
+++ notifi-cat-ion/notifi_cat_ionApp.swift
@@ -7,8 +7,34 @@
 
 import SwiftUI
 
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        
+        let catName = userInfo["catName"] as! String
+        let angerLevel = userInfo["angerLevel"] as! Int
+        
+        switch response.actionIdentifier {
+        case "FeedAction":
+            // Handle Feed Action tapped
+            break
+        case "IgnoreAction":
+            // Handle Ignore Action tapped
+            break
+        default: break
+        }
+        
+        completionHandler()
+    }
+}
+
 @main
 struct notifi_cat_ionApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
